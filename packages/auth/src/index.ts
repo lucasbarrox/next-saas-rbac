b6400b8b76d5b21835ff5c0be2482adcff4ddf80
Lucas Barros <lucasbandeiran@alu.ufc.br>
2025-10-02 01:00:14 -0300

    feat(auth): enhance role-based permissions with Zod schemas and add new subjects

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,10 +1,25 @@
-import { createMongoAbility, type ForcedSubject, type CreateAbility, type MongoAbility, AbilityBuilder } from '@casl/ability';
+import { createMongoAbility, type CreateAbility, type MongoAbility, AbilityBuilder } from '@casl/ability';
 import type { User } from './models/user';
 import { permissions } from './permissions';
-import type { UserSubject } from './subjects/user';
-import type { ProjectSubject } from './subjects/project';
+import { userSubject } from './subjects/user';
+import { projectSubject } from './subjects/project';
+import { z } from 'zod';
+import { organizationSubject } from './subjects/organization';
+import { inviteSubject } from './subjects/invite';
+import { billingSubject } from './subjects/billing';
 
-type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all'];
+
+const AppAbilitiesSchema = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+
+  z.tuple([z.literal('manage'), z.literal('all')])
+]);
+
+type AppAbilities = z.infer<typeof AppAbilitiesSchema>;
 
 export type AppAbility = MongoAbility<AppAbilities>;
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>; 
