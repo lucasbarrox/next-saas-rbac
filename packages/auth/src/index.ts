bed4c3d8179dd370ec45241b34c55724d4661cc5
Lucas Barros <lucasbandeiran@alu.ufc.br>
2025-10-01 23:37:55 -0300

    feat(auth): implement user role-based permissions and ability definition

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,4 +1,6 @@
 import { createMongoAbility, type ForcedSubject, type CreateAbility, type MongoAbility, AbilityBuilder } from '@casl/ability';
+import type { User } from './models/user';
+import { permissions } from './permissions';
 
 const actions = ['manage', 'invite', 'delete'] as const;
 const subjects = ['User', 'all'] as const;
@@ -10,9 +12,16 @@ type AppAbilities = [
 export type AppAbility = MongoAbility<AppAbilities>;
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>; 
 
-const {build, can, cannot} = new AbilityBuilder(createAppAbility)
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
 
-can('invite', 'User')
-cannot('delete', 'User')
+  if (typeof permissions[user.role] !== 'function') {
+    throw new Error(`Role ${user.role} does not have permissions assigned.`);
+  }
 
-export const ability = build()
+  permissions[user.role](user, builder)
+
+  const ability = builder.build()
+
+  return ability
+}
